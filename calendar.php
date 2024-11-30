521e32ed1bb7265e4f4eca83c847d623544c8a0a
miwanoff <marina.iva.noffa@gmail.com>
2024-11-30 13:21:17 +0200

    Add $firstDay

--- calendar.php
+++ calendar.php
@@ -3,10 +3,22 @@
 $daysOfWeek = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятница', 'Субота'];
 echo "Today " . date("d.m.Y, l") . "\n";
 $daysOfWeekN = date("w");
+$month = date("m");
+$year = date("Y");
 echo $daysOfWeek[$daysOfWeekN] . "\n";
 
-echo "<table>";
-for ($i=0; $i < ; $i++) { 
-    # code...
+echo "<table border='1'>";
+echo "<tr>";
+for ($i = 1; $i <= count($daysOfWeek); $i++) {
+    if ($i == 7) {
+        echo "<td>{$daysOfWeek[0]}</td>";
+    } else {
+        echo "<td>{$daysOfWeek[$i]}</td>";
+    }
+
 }
-echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td>";
+echo "</tr>";
+// echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td>";
+$firstDay = date("w", mktime(0, 0, 0, $month, 1, $year));
+
+echo $firstDay;
